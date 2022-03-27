5eb17af6be72ce39f99328faa00d9c7391e0e15f
rajesh.kumar@goavega.com-at-012114828737 <rajesh.kumar@goavega.com>
2022-03-27 07:33:12 +0530

    starter-project added!

---
+++ src/firebase-config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDM2ahnqVC24E0ikDG5VAmxIXmxfgtOq1k",
+  authDomain: "trello-clone-70062.firebaseapp.com",
+  projectId: "trello-clone-70062",
+  storageBucket: "trello-clone-70062.appspot.com",
+  messagingSenderId: "805609058394",
+  appId: "1:805609058394:web:cf6489a8929f7d6326a863",
+  measurementId: "G-HS1YVP5VH8",
+};
