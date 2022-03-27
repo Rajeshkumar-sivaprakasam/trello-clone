0dd8f5753b2c91d2079095901808d9c963ff407a
rajesh.kumar@goavega.com-at-012114828737 <rajesh.kumar@goavega.com>
2022-03-27 09:00:46 +0530

    navbar and home folder stuc added!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar'
 import './index.scss'
+
 
 export const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Home />
+    </div>
   )
 }
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import { Bookmark } from '@mui/icons-material'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container">
+        <div>
+          <Bookmark />
+          NavBar
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
